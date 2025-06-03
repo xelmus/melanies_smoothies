ac49e2a364562da891ede9486ebd4b95502d55a6
xelmus <xelmus@gmail.com>
2025-06-03 14:50:18 +0200

    Update streamlit_app.py
    add smoothiefroot to for fruit_chosen loop

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 # Import python packages
+import requests
 import streamlit as st
 from snowflake.snowpark.functions import col
 
@@ -29,6 +30,8 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
         
     #st.write(ingredients_string)
     
@@ -43,8 +46,3 @@ if ingredients_list:
             session.sql(my_insert_stmt).collect()
 
             st.success('Your Smoothie is ordered, '+ name_on_order+'!', icon="✅")
-
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
